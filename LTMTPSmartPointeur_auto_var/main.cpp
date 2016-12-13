a9e5bb9cee1ef55afa9e48c552ea198b31cda1bc
Bertrand Leclercq - Mac Book Pro <bl@ltm.fr>
2016-12-13 15:11:59 +0100

    Initial Commit

---
+++ LTMTPSmartPointeur_auto_var/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LTMTPSmartPointeur_auto_var
+//
+//  Created by bleclercq on 13/12/2016.
+//  Copyright © 2016 LTM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
